4cdcd5d4fc1c171eac438a29f6e88eb4b148f71b
Islam Dagaev <spy_002@bk.ru>
2023-06-22 18:59:06 +0500

    Выполнение функции задания

--- Final_Zadanie/Program.cs
+++ Final_Zadanie/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
-
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
@@ -11,6 +10,23 @@ void PrintArray(string[] array)
     Console.WriteLine();
 }
 
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+
 string[] array1 = new string[5] {"Final", "Zadanie", "Complited", "or", "No?"};
+string[] array2 = new string[array1.Length];
 
 PrintArray(array1);
+SecondArray(array1, array2);
+PrintArray(array2);
